7639dd457a27b8f54d901197d56ed76f5be70801
Dimitar Petrov <dimitarpetrov44@gmail.com>
2023-04-16 20:16:14 +0300

    Added creating and reading from a JSON file

---
+++ AutomatedYTChannel/VideoFiles.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AutomatedYTChannel
+{
+    internal class VideoFiles
+    {
+        string[] InputFiles;
+        string OutputFileName = "/result.mp4";
+        public static void ReadSettings()
+        {
+            if (!File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "/videopath.json"))
+            {
+                var DocumentsFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                var output = new Output
+                {
+                    OutputPath = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos)+"\\result.mp4"
+                };
+                var options = new JsonSerializerOptions { WriteIndented = true, IncludeFields = true };
+                string fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal)+"/videopath.json");
+                string json = JsonSerializer.Serialize(output, options);
+                File.WriteAllText(fileName, json);
+            }
+            string Json = File.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "/videopath.json");
+            Output output1 = JsonSerializer.Deserialize<Output>(Json);
+            Console.WriteLine(output1.OutputPath);
+
+            //sled kato se izbere papkata, se gledat vsichkite filove koito matchvat video filove i se dobavqt kym InputFiles
+            //sled tova shte callne SaveVideo funkciqta koqto da zadade ime i lokaciq na novoto video
+        }
+    }
+    internal class Output
+    {
+        [JsonInclude]
+        public string OutputPath { get; set; }
+    }
+}
